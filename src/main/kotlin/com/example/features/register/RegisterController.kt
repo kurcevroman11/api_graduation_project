16056b375f60e31a5af83db5a4cd55457b0f9da0
386 <romankurce4@gmail.com>
2023-05-22 13:32:14 +0300

    Removed unnecessary libraries

--- src/main/kotlin/com/example/features/register/RegisterController.kt
+++ src/main/kotlin/com/example/features/register/RegisterController.kt
@@ -1,20 +1,13 @@
 package com.example.features.register
 
-
-import com.auth0.jwt.JWT
-import com.auth0.jwt.algorithms.Algorithm
 import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.response.*
-import java.util.*
 import  com.example.database.user.usser
 import com.example.database.user.UsersDTO
 import com.example.plugins.generateTokenLong
 import com.example.plugins.generateTokenShort
-import com.example.utils.isValidPassword
-
 import io.ktor.server.request.*
-import org.jetbrains.exposed.sql.Database
 import org.jetbrains.exposed.sql.StdOutSqlLogger
 import org.jetbrains.exposed.sql.addLogger
 import org.jetbrains.exposed.sql.transactions.transaction
@@ -23,12 +16,7 @@ import org.jetbrains.exposed.sql.transactions.transaction
 class RegisterController(val call: ApplicationCall) {
 
     suspend fun registerNewUser(){
-
-
         val registerReciveRemote = call.receive<RegisterReciveRemote>()
-        if(!registerReciveRemote.password.isValidPassword()){
-            call.respond(HttpStatusCode.BadRequest, "Email is not")
-        }
 
         var tokenShort : String = ""
         var tokenLong : String = ""
