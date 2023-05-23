a37c61bfe12784f3629714542d81693fb0201ada
386 <romankurce4@gmail.com>
2023-05-23 15:44:06 +0300

    Add file with routing

---
+++ src/main/kotlin/com/example/database/user/UserRouting.kt
@@ -0,0 +1,60 @@
+package com.example.database.user
+
+import com.example.database.user.UsserModel.deleteUser
+import com.example.database.user.UsserModel.fetchAllUser
+import com.example.database.user.UsserModel.fetchUserID
+import com.example.database.user.UsserModel.updateUser
+import io.ktor.server.application.*
+import io.ktor.server.routing.*
+import com.google.gson.Gson
+import io.ktor.http.*
+import io.ktor.server.request.*
+import io.ktor.server.response.*
+
+fun Application.UserContriller() {
+    routing {
+        route("/User") {
+            get {
+                val usersDTO = fetchAllUser()
+                val gson = Gson()
+
+                val users = gson.toJson(usersDTO)
+
+                call.respond(users)
+            }
+
+            get("/{id}") {
+                val userId = call.parameters["id"]?.toIntOrNull()
+                if (userId != null) {
+                    val user = fetchUserID(userId)
+                    call.respond(user!!)
+                }else {
+                    call.respond(HttpStatusCode.BadRequest, "Invalid ID format.")
+                }
+            }
+
+            put("/{id}") {
+
+                val userId = call.parameters["id"]?.toIntOrNull()
+                if (userId != null) {
+                    val user = call.receive<String>()
+                    val gson = Gson()
+
+                    val userDTO = gson.fromJson(user, UsersDTO::class.java)
+                    call.respond(updateUser(userId, userDTO))
+                } else {
+                    call.respond(HttpStatusCode.BadRequest, "Invalid ID format.")
+                }
+            }
+
+            delete("/{id}") {
+                val userId = call.parameters["id"]?.toIntOrNull()
+                if (userId != null) {
+                    call.respond(deleteUser(userId), "Delete")
+                } else {
+                    call.respond(HttpStatusCode.BadRequest, "Invalid ID format.")
+                }
+            }
+        }
+    }
+}
